8a5590f43ea4553205c586c69d50380ea4d584de
dojoohwan <ehwnghks@gmail.com>
2024-04-07 18:17:38 +0900

    docker compose 파일 수정: volumes 설정 추가

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,14 +1,15 @@
-import { DataSource } from "typeorm";
+import { DataSource } from 'typeorm';
+import express from 'express';
 
 export const AppDataSource = new DataSource({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'mytv',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: false,
-    migrations: [__dirname + '/**/migrations/*.js'],
-    migrationsTableName: 'migrations',
-})
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'mytv',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [__dirname + '/**/migrations/*.js'],
+  migrationsTableName: 'migrations',
+});
